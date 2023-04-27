6c3abc4bbdd4d08545053657a525f812260dd37d
divermedic <divermedic22@netscape.net>
2023-04-27 00:03:53 +0000

    Ops challenge 03

---
+++ 201d8-code/201d8-day02.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script Name:                  Ops challenge 03
+# Author:                       Chris Bennett
+# Date of latest revision:      00/00/0000
+# Purpose:                      To practice coding
+
+# Declaration of variables
+
+# Declaration of functions
+# Write a function that prints the login history of users on this computer,
+print_something () {
+  last
+  # followed by the text “This is the login history”.
+echo "This is the login history"
+}
+
+
+# Main
+ 
+# In your script, call that function three times.
+print_something
+print_something
+print_something
+
+
+# Use the same template introduced in Challenge02.
+
+
+# End
