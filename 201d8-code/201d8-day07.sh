f68b41c7a9617e99582ad41a10cf8b891405cd8b
divermedic <divermedic22@netscape.net>
2023-05-03 14:32:00 +0000

    updated

--- 201d8-code/201d8-day07.sh
+++ 201d8-code/201d8-day07.sh
@@ -18,37 +18,28 @@
 
  # CPU
     # Product - Vendor - Physical ID - Bus info
-    sudo lshw | grep *-cpu -A 4
-     # Width
-     sudo lshw -C cpu | grep width:
+    # Uses grep to remove irrelevant information from the lshw output (used to remove version)
+    echo "CPU"
+    sudo lshw | grep *-cpu -A 6 | grep -vw "version"
+
 # RAM
     # Description - Physical ID - Size
+    echo "RAM"
     sudo lshw -c memory | grep memory -A 3    
 
 # Display adapter
     # Description - Product - Vendor - Physical ID - Bus info
+    echo "Display Adapter"
     sudo lshw | grep *-display -A 5
     # Width - Clock - Capabilities - Resources
     sudo lshw | grep *-display -A 12 | head -12 | tail -4
 
-  # Network adapter
-    # Description
-    # Product
-    # Vendor
-    # Physical ID
-    # Bus info
-    # Logical name
-    # Version
-    # Serial
-    # Size
-    # Capacity
-    # Width
-    # Clock
-    # Capabilities
-    # Configuration
-    # Resources
-
-# Uses grep to remove irrelevant information from the lshw output
+# Network adapter
+    # Description - Product - Vendor - Physical ID - Bus info
+        # Logical name - Version - Serial - Size - Capacity - Width
+        # Clock - Capabilities - Configuration - Capabilities - Resources
+    echo "Network Adapter"    
+    sudo lshw | grep *-network -A 15
 
 # Add text to the output clearly indicating which component (such as CPU, RAM, etc.) the script is returning information about
 
