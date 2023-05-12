13da43090a879f90d11e121ec568f879dac7121a
divermedic <divermedic22@netscape.net>
2023-05-12 04:16:23 +0000

    Readme for this repository

---
+++ 201d8-code/readme.sh
@@ -0,0 +1,21 @@
+#Q. What is this repo all about?
+#A. This is the repository for my coding/scripting challenges in Code Fellows Ops 201 course.
+
+#Q. Why are you learning these skills?
+#A. Having at least a basic understanding of coding and scripting gives a basis for understanding how computers "think" and how certain scripts make them behave. As I progress in cybersecurity training and experience, this knowledge can assit me in detected and finding malicious attacks by recognizing the behaviors. 
+
+#Q. What fundamental concepts did you learn in Ops 201?
+#Ops 201 taught me:
+ # * Fundamental IT operations such as troubleshooting for computer hardware and software, with associated tools.
+#  * Different operating systems, particularly Linux and Windows
+#  * Basic coding and writing scripts in bash and powershell, to include operations such as loops, variables, functions and other coding tools
+#  * Introduction to some topics that will be learned in more detail later, such as networking and cybersecurity
+
+#Q. What was your favorite Ops Challenge assignment and why?
+#A. '; 
+
+#How can these examples of shell scripting be applied to the workplace?
+
+#What scripts would you continue to improve if you had the time?
+
+#What aspect of your shell scripting skillset do you hope to improve next?
